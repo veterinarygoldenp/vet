e1088bfcb625dcf1cb3834528d8829093cbc09bf
veterinarygoldenp <dr.vet4.sayed@gmail.com>
2025-04-04 15:23:02 -0700

    launch.json

---
+++ js/app.js
@@ -0,0 +1,15 @@
+{
+    "configurations": [
+      {
+        "type": "node",
+        "request": "launch",
+        "name": "Launch Webpack Dev Server",
+        "program": "${workspaceFolder}/node_modules/webpack-dev-server/bin/webpack-dev-server.js",
+        "args": [
+          "--config",
+          "${workspaceFolder}/webpack.config.dev.js"
+        ],
+        "preLaunchTask": "npm: start"
+      }
+    ]
+  }
